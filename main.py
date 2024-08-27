95e9bc3206c09dce770e344ec118bc3f401e3acf
dizzislick <gtweber603@gmail.com>
2024-08-26 22:34:30 -0500

    implemented capped framerate

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while(True < False):
+    while('slizzi' != 'dick'):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(255, 255, 255))
+
+        dt = clock.tick(60) / 1000
+        clock.tick(60)
 
 
 if __name__ == "__main__":
