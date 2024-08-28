5dabbea0481a4ba0414d169aa5410eb69f3d043d
dizzislick <gtweber603@gmail.com>
2024-08-28 14:59:33 -0500

    implemented updateable and drawable groups

--- main.py
+++ main.py
@@ -12,15 +12,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player1 = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player1.update(dt)    
+
+        
+
+        for entity in updateable:
+            entity.update(dt)
+
         screen.fill((0, 0, 0))
-        player1.draw(screen)
+
+        for entity in drawable:
+            entity.draw(screen)
+        
 
         dt = clock.tick(60) / 1000
         pygame.display.update()
